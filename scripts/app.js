4bd09507cf82486fbbd4f448a6aab3e689b0bd07
Szymon Dolnik <szymon2407@hotmail.com>
2021-07-12 01:19:51 +0200

    Add basic feature functionality, need to add custom values

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+const billForm = document.querySelector('.bill-form');
+const selectTipInputs = document.querySelectorAll('input[type="radio"]');
+const customTipInput = document.querySelector('.tip-custom');
+const peopleNRForm = document.querySelector('.peoplenr-form');
+const tipOutput = document.querySelector('#tip-output');
+const totalOutput = document.querySelector('#total-output');
+const mainElement = document.querySelector('main');
+
+
+const calculator = new tipCalculator(tipOutput, totalOutput);
+
+mainElement.addEventListener('change', e => {
+
+  const billValue = billForm[0].value.trim();
+  const peopleValue = peopleNRForm[0].value.trim();
+
+  selectTipInputs.forEach(tipInput => {
+    if (tipInput.checked) {
+      const tipValue = tipInput.attributes[3].value.trim();
+
+      calculator.init(billValue, tipValue, peopleValue)
+    }
+  })
+})
+
+mainElement.addEventListener('submit', e => {
+  e.preventDefault();
+})
+
+mainElement.addEventListener('click', e => {
+  if (e.target.classList.contains('reset')) {
+    calculator.reset();
+    billForm.reset();
+    peopleNRForm.reset();
+    customTipInput.value = '';
+  }
+});
---
+++ scripts/calculate.js
@@ -0,0 +1,26 @@
+class tipCalculator {
+  constructor(tipOutput, totalOutput) {
+    this.tipOutput = tipOutput;
+    this.totalOutput = totalOutput;
+    this.reset();
+  }
+  reset() {
+    this.tipOutput.innerText = '';
+    this.totalOutput.innerText = '';
+  }
+  init(bill, tip, people) {
+    if (bill === '' || bill === '0' || isNaN(bill)) return
+    if (tip === '' || tip === '0' || isNaN(tip)) return
+    if (people === '' || people === '0' || isNaN(people)) return
+
+    this.value1 = (bill * tip) / people
+    this.value2 = (bill / people) + this.value1
+
+    this.updateUI(this.value1.toString(), this.value2.toString())
+  }
+
+  updateUI(value1, value2) {
+    this.tipOutput.innerText = `$${value1}`;
+    this.totalOutput.innerText = `$${value2}`;
+  }
+}
